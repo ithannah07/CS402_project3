6cedc1c335252acd7b1e0af5d78698fa733a7b22
Sunho Kim <107231365+sunhome243@users.noreply.github.com>
2026-03-30 16:38:36 -0400

    Fix comment in gen_cert.sh for clarity on self-signed certificates

--- gen_cert.sh
+++ gen_cert.sh
@@ -2,4 +2,4 @@
 openssl req -x509 -newkey rsa:2048 -keyout server.key -out server.crt -days 365 -nodes -subj "/CN=localhost"
 openssl req -x509 -newkey rsa:2048 -keyout client.key -out client.crt -days 365 -nodes -subj "/CN=client"
 
-# confirmed that no CA is needed. using manual certs and keys without CA. Much simpler. Added #!/bin/bash to run as a shell script.
+# confirmed that no CA is needed. using manual self signed certs and keys without CA. This is much simpler. Added #!/bin/bash to run as a shell script.
